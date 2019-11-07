0bb63d04362322d3c6a092d4cb1d16c41a13b015
Sangeeta12345 <sangeetapanigrahy5@gmail.com>
2019-11-07 09:42:38 +0530

    secondCommit

--- src/abs/com/LoginForm.java
+++ src/abs/com/LoginForm.java
@@ -12,30 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginForm extends HttpServlet{
          private Connection cn;
-		 public void init(){
-			
-			 
-			 try{
-				 Class.forName("oracle.jdbc.driver.OracleDriver");
-				 cn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");
-			 }
-
-
-			 catch(Exception e){
-				 e.printStackTrace();
-			 }
-		 }
 	
+			
+			
       protected void doPost(HttpServletRequest req, HttpServletResponse res)throws ServletException,IOException{
+    	  try {
+    	  Class.forName("oracle.jdbc.driver.OracleDriver");
+		  cn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");
 		  String ffname=req.getParameter("uid");
           String mmname=req.getParameter("mid");
           String llname=req.getParameter("lid");
           String genders=req.getParameter("gender");
-		  int phnoe=Integer.parseInt("phn");
+		  long phnoe=Long.parseLong(req.getParameter("phn"));
 
 
           PrintWriter out=res.getWriter();
-        try {  
+        
 		  PreparedStatement ps=cn.prepareStatement("insert into UserTable values(?,?,?,?,?)");
 		
 
@@ -43,14 +35,17 @@ public class LoginForm extends HttpServlet{
 		  ps.setString(2,mmname);
           ps.setString(3,llname);
           ps.setString(4,genders);
-          ps.setInt(5,phnoe);
-		  ps.executeUpdate();
+          ps.setLong(5,phnoe);
+		  int re =  ps.executeUpdate();
+		  if(re == 1) 
 		  out.println("Data Inserted");
+		  else
+			  out.println("data not inserted");
 	  }
        
 
 	    catch(Exception e){
-				 out.println("Error in Registering User");
+				 System.out.println("Exception thrown from try");
 			 }
 		 }
 }
